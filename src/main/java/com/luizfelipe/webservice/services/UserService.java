e450a5741df9d116152a874e7334d6170a3f760b
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-12-26 18:25:33 -0300

    User insert

--- src/main/java/com/luizfelipe/webservice/services/UserService.java
+++ src/main/java/com/luizfelipe/webservice/services/UserService.java
@@ -4,6 +4,7 @@ import com.luizfelipe.webservice.entities.User;
 import com.luizfelipe.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +22,9 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
         return obj.get();
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 }
