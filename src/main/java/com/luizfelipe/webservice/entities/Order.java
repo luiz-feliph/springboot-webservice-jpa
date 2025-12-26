1216bfe90ab1fffbc9a1c6d8d2ac7a13678eb559
Luiz Felipe <luizfelipe01.2007@gmail.com>
2025-12-26 18:09:58 -0300

    Subtotal and Total methods

--- src/main/java/com/luizfelipe/webservice/entities/Order.java
+++ src/main/java/com/luizfelipe/webservice/entities/Order.java
@@ -90,6 +90,12 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        return items.stream()
+                .map(OrderItem::getSubTotal)
+                .reduce(.0, Double::sum);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
