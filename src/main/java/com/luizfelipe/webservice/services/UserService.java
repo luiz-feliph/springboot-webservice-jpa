e87c686c3986e2c0eb18d4dfde4b74200d24e99b
Luiz Felipe <luizfelipe01.2007@gmail.com>
2026-01-03 22:36:13 -0300

    Exception handling - update

--- src/main/java/com/luizfelipe/webservice/services/UserService.java
+++ src/main/java/com/luizfelipe/webservice/services/UserService.java
@@ -4,13 +4,11 @@ import com.luizfelipe.webservice.entities.User;
 import com.luizfelipe.webservice.repositories.UserRepository;
 import com.luizfelipe.webservice.services.exceptions.DataBaseException;
 import com.luizfelipe.webservice.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 
-import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 
@@ -48,8 +46,12 @@ public class UserService {
     }
 
     public User update(Long id, User requestData) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, requestData);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, requestData);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 }
